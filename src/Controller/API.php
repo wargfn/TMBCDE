bfaf89edc3ec604369e1133f52fb00fd425aecb0
wargfn <wargfn@hotmail.com>
2019-06-17 16:36:17 -0400

    Added All of the Generation elements

--- src/Controller/API.php
+++ src/Controller/API.php
@@ -1462,19 +1462,18 @@ class API extends AbstractFOSRestController
 
         return $this->handleView($this->view($buildEncounter));
     }
-
     /**
      * Generates a Challenge
-     * @Rest\Get("/generateEncounterAll")
+     * @Rest\Get("/generateSliceDice")
      *
      * @return Response
      */
-    public function getGenerateEncounterAllAction()
+    public function getGenerateSliceDiceAction()
     {
         $cards = $this->getDoctrine()->getRepository(Cards::class);
         $tyrants = $this->getDoctrine()->getRepository(Tyrants::class);
         $encounters = $this->getDoctrine()->getRepository(Encounterlists::class);
-        $rndNum = rand(1, 13);
+        $rndNum = rand(18, 23);
         if ($rndNum == 12) {
             $rndNum++;
         }
@@ -1490,8 +1489,476 @@ class API extends AbstractFOSRestController
         if ($rndNum < 8)
         {
             array_splice($encCards, count($encCards), 0, $soloBaseCards);
-        }else {
+        }elseif ($rndNum > 7 && $rndNum < 14) {
             array_splice($encCards, count($encCards), 0, $soloUndertowCards);
+        } else {
+            array_splice($encCards, count($encCards), 0, $soloBaseCards);
+            array_splice($encCards, count($encCards), 0, $soloUndertowCards);
+
+        }
+        $buildEncounter = $encounters->findOneBy(['id' => '1']);
+        $buildEncounter->setTyrantId($rndTyrant);
+        if ($days >= 1) {
+            if ($rndNum < 8) {
+                $age = rand(0,1);
+                if ($age == 0) {
+                    $buildEncounter->setDay1Id($cards->findOneBy(['id' => '31']));
+                } else {
+                    $day1Num = rand(182, 188);
+                    $buildEncounter->setDay1Id($cards->findOneBy(['id' => $day1Num]));
+                }
+            } else {
+                $age = rand(0,1);
+                if ($age ==0) {
+                    $day1Num = rand(91, 94);
+                    $buildEncounter->setDay1Id($cards->findOneBy(['id' => $day1Num]));
+                } else {
+                    $day1Num = rand(182, 188);
+                    $buildEncounter->setDay1Id($cards->findOneBy(['id' => $day1Num]));
+                }
+            }
+        }
+
+        if ($days >= 2) {
+            if ($rndNum < 8) {
+                $age = rand(0,1);
+                if ($age == 0) {
+                    $buildEncounter->setDay2Id($cards->findOneBy(['id' => '32']));
+                } else {
+                    $day2Num = rand(189, 195);
+                    $buildEncounter->setDay2Id($cards->findOneBy(['id' => $day2Num]));
+                }
+            } else {
+                $age = rand(0, 1);
+                if ($age == 0) {
+                    $day2Num = rand(95, 98);
+                    $buildEncounter->setDay2Id($cards->findOneBy(['id' => $day2Num]));
+                } else {
+                    $day2Num = rand(189, 195);
+                    $buildEncounter->setDay2Id($cards->findOneBy(['id' => $day2Num]));
+                }
+            }
+        }
+
+        if ($days >= 3) {
+            if ($rndNum < 8) {
+                $age = rand(0,1);
+                if($age == 0 ){
+                    $buildEncounter->setDay3Id($cards->findOneBy(['id' => '33']));
+                } else {
+                    $day3Num = rand(196, 202);
+                    $buildEncounter->setDay3Id($cards->findOneBy(['id' => $day3Num]));
+                }
+            } else {
+                $age = rand(0,1);
+                if($age == 0) {
+                    $randCardNum = rand(0, count($encCards));
+                    while (!isset($encCards[$randCardNum])) {
+                        $randCardNum = rand(0, count($encCards));
+                    }
+                    $buildEncounter->setDay3Id($encCards[$randCardNum]);
+                    unset($encCards[$randCardNum]);
+                    $ar2 = array_values($encCards);
+                    $encCards = $ar2;
+                } else {
+                    $day3Num = rand(196, 202);
+                    $buildEncounter->setDay3Id($cards->findOneBy(['id' => $day3Num]));
+                }
+            }
+
+        }
+
+        if ($days >= 4) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay4Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 5) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay5Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 6) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay6Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 7) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay7Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 8) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay8Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 9) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay9Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 10) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay10Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 11) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay11Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 12) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay12Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 13) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay13Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+        //$results = $soloCards;
+        //$results = $rndTyrant;
+
+        return $this->handleView($this->view($buildEncounter));
+    }
+
+    /**
+     * Generates a Challenge
+     * @Rest\Get("/generateGeneralSliceDice")
+     *
+     * @return Response
+     */
+    public function getGenerateGeneralSliceDiceAction()
+    {
+        $cards = $this->getDoctrine()->getRepository(Cards::class);
+        $tyrants = $this->getDoctrine()->getRepository(Tyrants::class);
+        $encounters = $this->getDoctrine()->getRepository(Encounterlists::class);
+        $rndNum = rand(18, 23);
+        if ($rndNum == 12) {
+            $rndNum++;
+        }
+        $general40Cards = $cards->findBy(['cardType' => 'Solo', 'cardSet' => '40 Days']);
+        $generalBaseCards = $cards->findBy(['cardType' => 'Solo', 'cardSet' => 'Base Set']);
+        $generalUndertowCards = $cards->findBy(['cardType' => 'Solo', 'cardSet' => 'Undertow']);
+        $rndTyrant = $tyrants->findOneBy(['id' => $rndNum]);
+        $days = $rndTyrant->getDays();
+        $tyrant = $rndTyrant->getName();
+        $tyrantCards = $cards->findBy(['cardType' => $tyrant]);
+        $encCards = $general40Cards;
+        array_splice($encCards, count($encCards), 0, $tyrantCards);
+        if ($rndNum < 8)
+        {
+            array_splice($encCards, count($encCards), 0, $generalBaseCards);
+        }elseif ($rndNum > 7 && $rndNum < 14) {
+            array_splice($encCards, count($encCards), 0, $generalUndertowCards);
+        } else {
+            array_splice($encCards, count($encCards), 0, $generalBaseCards);
+            array_splice($encCards, count($encCards), 0, $generalUndertowCards);
+
+        }
+        $buildEncounter = $encounters->findOneBy(['id' => '1']);
+        $buildEncounter->setTyrantId($rndTyrant);
+        if ($days >= 1) {
+            if ($rndNum < 8) {
+                $age = rand(0,1);
+                if ($age == 0) {
+                    $buildEncounter->setDay1Id($cards->findOneBy(['id' => '31']));
+                } else {
+                    $day1Num = rand(182, 188);
+                    $buildEncounter->setDay1Id($cards->findOneBy(['id' => $day1Num]));
+                }
+            } else {
+                $age = rand(0,1);
+                if ($age ==0) {
+                    $day1Num = rand(91, 94);
+                    $buildEncounter->setDay1Id($cards->findOneBy(['id' => $day1Num]));
+                } else {
+                    $day1Num = rand(182, 188);
+                    $buildEncounter->setDay1Id($cards->findOneBy(['id' => $day1Num]));
+                }
+            }
+        }
+
+        if ($days >= 2) {
+            if ($rndNum < 8) {
+                $age = rand(0,1);
+                if ($age == 0) {
+                    $buildEncounter->setDay2Id($cards->findOneBy(['id' => '32']));
+                } else {
+                    $day2Num = rand(189, 195);
+                    $buildEncounter->setDay2Id($cards->findOneBy(['id' => $day2Num]));
+                }
+            } else {
+                $age = rand(0, 1);
+                if ($age == 0) {
+                    $day2Num = rand(95, 98);
+                    $buildEncounter->setDay2Id($cards->findOneBy(['id' => $day2Num]));
+                } else {
+                    $day2Num = rand(189, 195);
+                    $buildEncounter->setDay2Id($cards->findOneBy(['id' => $day2Num]));
+                }
+            }
+        }
+
+        if ($days >= 3) {
+            if ($rndNum < 8) {
+                $age = rand(0,1);
+                if($age == 0 ){
+                    $buildEncounter->setDay3Id($cards->findOneBy(['id' => '33']));
+                } else {
+                    $day3Num = rand(196, 202);
+                    $buildEncounter->setDay3Id($cards->findOneBy(['id' => $day3Num]));
+                }
+            } else {
+                $age = rand(0,1);
+                if($age == 0) {
+                    $randCardNum = rand(0, count($encCards));
+                    while (!isset($encCards[$randCardNum])) {
+                        $randCardNum = rand(0, count($encCards));
+                    }
+                    $buildEncounter->setDay3Id($encCards[$randCardNum]);
+                    unset($encCards[$randCardNum]);
+                    $ar2 = array_values($encCards);
+                    $encCards = $ar2;
+                } else {
+                    $day3Num = rand(196, 202);
+                    $buildEncounter->setDay3Id($cards->findOneBy(['id' => $day3Num]));
+                }
+            }
+
+        }
+
+
+        if ($days >= 4) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay4Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 5) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay5Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 6) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay6Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 7) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay7Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 8) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay8Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 9) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay9Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 10) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay10Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 11) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay11Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 12) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay12Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+
+        if ($days >= 13) {
+            $randCardNum = rand(0, count($encCards));
+            while (!isset($encCards[$randCardNum])) {
+                $randCardNum = rand(0, count($encCards));
+            }
+            $buildEncounter->setDay13Id($encCards[$randCardNum]);
+            unset($encCards[$randCardNum]);
+            $ar2 = array_values($encCards);
+            $encCards = $ar2;
+
+        }
+        //$results = $soloCards;
+        //$results = $rndTyrant;
+
+        return $this->handleView($this->view($buildEncounter));
+    }
+
+
+    /**
+     * Generates a Challenge
+     * @Rest\Get("/generateEncounterAll")
+     *
+     * @return Response
+     */
+    public function getGenerateEncounterAllAction()
+    {
+        $cards = $this->getDoctrine()->getRepository(Cards::class);
+        $tyrants = $this->getDoctrine()->getRepository(Tyrants::class);
+        $encounters = $this->getDoctrine()->getRepository(Encounterlists::class);
+        $rndNum = rand(1, 23);
+        if ($rndNum == 12) {
+            $rndNum++;
+        }
+        $solo40Cards = $cards->findBy(['cardType' => 'Solo', 'cardSet' => '40 Days']);
+        $soloBaseCards = $cards->findBy(['cardType' => 'Solo', 'cardSet' => 'Base Set']);
+        $soloUndertowCards = $cards->findBy(['cardType' => 'Solo', 'cardSet' => 'Undertow']);
+        $rndTyrant = $tyrants->findOneBy(['id' => $rndNum]);
+        $days = $rndTyrant->getDays();
+        $tyrant = $rndTyrant->getName();
+        $tyrantCards = $cards->findBy(['cardType' => $tyrant]);
+        $encCards = $solo40Cards;
+        array_splice($encCards, count($encCards), 0, $tyrantCards);
+        if ($rndNum < 8)
+        {
+            array_splice($encCards, count($encCards), 0, $soloBaseCards);
+        }elseif ($rndNum > 7 && $rndNum < 14) {
+            array_splice($encCards, count($encCards), 0, $soloUndertowCards);
+        } else {
+            array_splice($encCards, count($encCards), 0, $soloBaseCards);
+            array_splice($encCards, count($encCards), 0, $soloUndertowCards);
+
         }
         $buildEncounter = $encounters->findOneBy(['id' => '1']);
         $buildEncounter->setTyrantId($rndTyrant);
@@ -1701,7 +2168,7 @@ class API extends AbstractFOSRestController
         $cards = $this->getDoctrine()->getRepository(Cards::class);
         $tyrants = $this->getDoctrine()->getRepository(Tyrants::class);
         $encounters = $this->getDoctrine()->getRepository(Encounterlists::class);
-        $rndNum = rand(1, 13);
+        $rndNum = rand(1, 23);
         if ($rndNum == 12) {
             $rndNum++;
         }
@@ -1717,8 +2184,12 @@ class API extends AbstractFOSRestController
         if ($rndNum < 8)
         {
             array_splice($encCards, count($encCards), 0, $generalBaseCards);
-        }else {
+        }elseif ($rndNum > 7 && $rndNum < 14) {
             array_splice($encCards, count($encCards), 0, $generalUndertowCards);
+        } else {
+            array_splice($encCards, count($encCards), 0, $generalBaseCards);
+            array_splice($encCards, count($encCards), 0, $generalUndertowCards);
+
         }
         $buildEncounter = $encounters->findOneBy(['id' => '1']);
         $buildEncounter->setTyrantId($rndTyrant);
