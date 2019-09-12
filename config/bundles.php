cbddab7a9a106bff17b668c016f03f0e5d4e5a0e
wargfn <wargfn@hotmail.com>
2019-09-12 10:23:36 -0400

    FOSUserBundle Functioning

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     FOS\OAuthServerBundle\FOSOAuthServerBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
